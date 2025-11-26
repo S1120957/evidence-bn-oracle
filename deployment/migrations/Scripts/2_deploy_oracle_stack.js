abcf4e70059d0f392a9ce0fa0b8fb9379893174a
t.naeem <S1120957@pm.univpm.it>
2025-11-26 03:37:58 +0100

    Update 2_deploy_oracle_stack.js

--- deployment/migrations/Scripts/2_deploy_oracle_stack.js
+++ deployment/migrations/Scripts/2_deploy_oracle_stack.js
@@ -1,1 +1,12 @@
+const CPTStore = artifacts.require("CPTStore");
+const EvidenceRegistry = artifacts.require("EvidenceRegistry");
+const OracleController = artifacts.require("OracleController");
 
+module.exports = async function (deployer, network, accounts) {
+  await deployer.deploy(EvidenceRegistry);
+  const evidenceRegistry = await EvidenceRegistry.deployed();
+
+  const cptStore = await CPTStore.deployed();
+
+  await deployer.deploy(OracleController, cptStore.address, evidenceRegistry.address);
+};
